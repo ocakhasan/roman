3a70c953f970ec88797f640e3c5160aaab1ad600
Hasan Ocak <ocakhasan99@gmail.com>
2022-04-05 11:15:08 +0300

    add test cases for the range conversion case

--- pkg/handler/handler_test.go
+++ pkg/handler/handler_test.go
@@ -118,3 +118,133 @@ func TestHandler_RomanNumeralConversion(t *testing.T) {
 		})
 	}
 }
+
+func TestHandler_MinMaxRangeConversions(t *testing.T) {
+	ts := newTestServer()
+	defer ts.Close()
+	client := ts.Client()
+
+	cases := []struct {
+		name             string
+		httpMethod       string
+		minQuery         string
+		maxQuery         string
+		statusCode       int
+		expectedResponse interface{}
+		errHappened      bool
+	}{
+		{
+			name:       "empty min query input",
+			httpMethod: http.MethodPost,
+			minQuery:   "",
+			maxQuery:   "12",
+			statusCode: http.StatusBadRequest,
+			expectedResponse: handlerError.ErrorResponse{
+				Err:        handlerError.ErrEmptyInput.Error(),
+				StatusCode: http.StatusBadRequest,
+			},
+			errHappened: true,
+		},
+		{
+			name:       "invalid min or max query input",
+			httpMethod: http.MethodPost,
+			minQuery:   "1",
+			maxQuery:   "12asdas",
+			statusCode: http.StatusBadRequest,
+			expectedResponse: handlerError.ErrorResponse{
+				Err:        handlerError.ErrInvalidInput.Error(),
+				StatusCode: http.StatusBadRequest,
+			},
+			errHappened: true,
+		},
+		{
+			name:       "min is bigger than max",
+			httpMethod: http.MethodPost,
+			minQuery:   "12",
+			maxQuery:   "2",
+			statusCode: http.StatusBadRequest,
+			expectedResponse: handlerError.ErrorResponse{
+				Err:        handlerError.ErrMinBiggerThanMax.Error(),
+				StatusCode: http.StatusBadRequest,
+			},
+			errHappened: true,
+		},
+		{
+			name:       "valid min max query",
+			httpMethod: http.MethodPost,
+			minQuery:   "1",
+			maxQuery:   "5",
+			statusCode: http.StatusOK,
+			expectedResponse: structure.RangeResponse{Conversions: []structure.RomanResponse{
+				{
+					Input:  "1",
+					Output: "I",
+				},
+				{
+					Input:  "2",
+					Output: "II",
+				},
+				{
+					Input:  "3",
+					Output: "III",
+				},
+				{
+					Input:  "4",
+					Output: "IV",
+				},
+				{
+					Input:  "5",
+					Output: "V",
+				},
+			}},
+			errHappened: false,
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			url := fmt.Sprintf("%s/romannumeral", ts.URL)
+			if tt.minQuery != "" {
+				url = fmt.Sprintf("%s?min=%s&", url, tt.minQuery)
+			}
+
+			if tt.maxQuery != "" {
+				if tt.minQuery == "" {
+					url = fmt.Sprintf("%s?max=%s", url, tt.maxQuery)
+				} else {
+					url = fmt.Sprintf("%smax=%s", url, tt.maxQuery)
+				}
+			}
+
+			request, err := http.NewRequest(tt.httpMethod, url, nil)
+			if err != nil {
+				t.Errorf("unexpected error %v\n", err)
+			}
+
+			request.Header.Set("Referer", "http://localhost")
+
+			response, err := client.Do(request)
+			if err != nil {
+				t.Errorf("unexpected response error %v\n", err)
+			}
+
+			if response.StatusCode != tt.statusCode {
+				t.Errorf("expected statusCode :%v, got: %v\n", tt.statusCode, response.StatusCode)
+			}
+
+			if tt.errHappened {
+				var resp handlerError.ErrorResponse
+				if err := json.NewDecoder(response.Body).Decode(&resp); err != nil {
+					t.Errorf("error while decoding the error response body: %v", err)
+				}
+				assert.Equal(t, resp, tt.expectedResponse)
+			} else {
+				var resp structure.RangeResponse
+				if err := json.NewDecoder(response.Body).Decode(&resp); err != nil {
+					t.Errorf("error while decoding the vehicle response body: %v", err)
+				}
+				assert.Equal(t, resp, tt.expectedResponse)
+			}
+		})
+	}
+}
