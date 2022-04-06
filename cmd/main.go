9d003fa045ac5c2d2836c80df3ce483a910b26da
Hasan Ocak <ocakhasan99@gmail.com>
2022-04-06 21:52:46 +0300

    fix the docker-compose and make sure that everything works as expected

--- cmd/main.go
+++ cmd/main.go
@@ -20,6 +20,8 @@ func main() {
 		ctx = context.Background()
 	)
 
+	l.SetFormatter(&logrus.JSONFormatter{})
+
 	httpPort := os.Getenv("PORT") // read the env
 	if httpPort == "" {
 		httpPort = "8080"
